f0f8f394b36561e22bb8ff0700395d6cbb6780ed
Steven Tattersall <tattlemuss@github.com>
2020-08-23 21:02:26 +0100

    Add -unnamed-sections to build script to support latest vasm versions.

--- src/build.sh
+++ src/build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 # Build script to build using vasm (http://sun.hasenbraten.de/vasm/) for Atari platforms.
-vasmm68k_mot lz4_test.s -devpac -Felf -o lz4_test.o
+vasmm68k_mot lz4_test.s -devpac -Felf -unnamed-sections -o lz4_test.o
 vlink lz4_test.o -b ataritos -o lz4_test.prg
 
-vasmm68k_mot lzsa_test.s -devpac -Felf -o lzsa_test.o
+vasmm68k_mot lzsa_test.s -devpac -Felf -unnamed-sections  -o lzsa_test.o
 vlink lzsa_test.o -b ataritos -o lzsa_test.prg
 
